152b2e358154f5ee129c445be5efaacd7ce321b3
ayuan <ayuan2500@163.com>
2020-11-11 11:58:39 +0800

    核心工程

--- changgou-parent/changgou-service/changgou-service-goods/src/main/java/com/changgou/goods/service/BrandService.java
+++ changgou-parent/changgou-service/changgou-service-goods/src/main/java/com/changgou/goods/service/BrandService.java
@@ -1,9 +1,7 @@
 package com.changgou.goods.service;
 
+import com.changgou.core.service.CoreService;
 import com.changgou.goods.pojo.Brand;
-import com.github.pagehelper.PageInfo;
-
-import java.util.List;
 
 /***
  * @ClassName BrandService
@@ -12,28 +10,5 @@ import java.util.List;
  * @author ayuan
  * @createTime 2020/11/09 21:31:00
  */
-public interface BrandService {
-    //查询所有品牌信息
-    List<Brand> selectAll();
-
-    //根据品牌id查询品牌信息
-    Brand findById(Integer id);
-
-    //新增品牌
-    void add(Brand brand);
-
-    //根据品牌id修改品牌信息
-    void update(Brand brand);
-
-    //根据品牌id删除品牌信息
-    void delete(Integer id);
-
-    //根据条件搜索品牌
-    List<Brand> findList(Brand brand);
-
-    //分页查询
-    PageInfo<Brand> findPage(int page, int size);
-
-    //根据搜索条件分页查询
-    PageInfo<Brand> findPage(Brand brand, int page, int size);
+public interface BrandService extends CoreService<Brand> {
 }
